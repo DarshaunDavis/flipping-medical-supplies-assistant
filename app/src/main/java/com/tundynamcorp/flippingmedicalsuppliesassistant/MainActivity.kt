24d7300339e45fb47dba0b9d970eaa6ba735d17d
Darshaun Davis <34321400+DarshaunDavis@users.noreply.github.com>
2025-04-20 12:52:02 -0400

    chore: add Android module files (Gradle configs, manifest, resources, source)

---
+++ app/src/main/java/com/tundynamcorp/flippingmedicalsuppliesassistant/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.tundynamcorp.flippingmedicalsuppliesassistant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tundynamcorp.flippingmedicalsuppliesassistant.ui.theme.FlippingMedicalSuppliesAssistantTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FlippingMedicalSuppliesAssistantTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FlippingMedicalSuppliesAssistantTheme {
+        Greeting("Android")
+    }
+}
